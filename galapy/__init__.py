29f335434c10e4a270ecf6cd33b8a74ce327cd75
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2021-11-16 13:58:21 +0100

    added imports to init file

--- galapy/__init__.py
+++ galapy/__init__.py
@@ -1,24 +1,28 @@
 """ This package simulates galactic spectra
 """
 
-__all__ = [ 'StarFormationHistory' ]
+import galapy.StarFormationHistory
+import galapy.CompositeStellarPopulation
+import galapy.InterStellarMedium
 
-import galapy.internal.globs as GP_GBL
-import os 
+__all__ = [ 'StarFormationHistory', 'CompositeStellarPopulation', 'InterStellarMedium' ]
 
-SSP_LIB = {
-    'bc03.basel.chab.extend' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
-                                              GP_GBL.bc03_basel_chab_zeros ),
-    'bc03.stelib.chab.extend' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
-                                              GP_GBL.bc03_stelib_chab_zeros ),
-    'bc03.stelib.chab.extrap' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
-                                              GP_GBL.bc03_stelib_chab_extrap ),
-    }
-""" Dictionary providing available SSP-libraries.
+# import galapy.internal.globs as GP_GBL
+# import os 
 
-Available SSP formats:
-----------------------
-* bc03.basel.chab.extend :
-* bc03.stelib.chab.(extend/extrap) :
-"""
+# SSP_LIB = {
+#     'bc03.basel.chab.extend' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
+#                                               GP_GBL.bc03_basel_chab_zeros ),
+#     'bc03.stelib.chab.extend' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
+#                                               GP_GBL.bc03_stelib_chab_zeros ),
+#     'bc03.stelib.chab.extrap' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
+#                                               GP_GBL.bc03_stelib_chab_extrap ),
+#     }
+# """ Dictionary providing available SSP-libraries.
+
+# Available SSP formats:
+# ----------------------
+# * bc03.basel.chab.extend :
+# * bc03.stelib.chab.(extend/extrap) :
+# """
 
