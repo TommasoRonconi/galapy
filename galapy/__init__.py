bc5ae12d8275653d6c535c12eeea2ef14e607fa8
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2021-11-22 11:58:35 +0100

    removed useless commented section

--- galapy/__init__.py
+++ galapy/__init__.py
@@ -7,22 +7,3 @@ import galapy.InterStellarMedium
 
 __all__ = [ 'StarFormationHistory', 'CompositeStellarPopulation', 'InterStellarMedium' ]
 
-# import galapy.internal.globs as GP_GBL
-# import os 
-
-# SSP_LIB = {
-#     'bc03.basel.chab.extend' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
-#                                               GP_GBL.bc03_basel_chab_zeros ),
-#     'bc03.stelib.chab.extend' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
-#                                               GP_GBL.bc03_stelib_chab_zeros ),
-#     'bc03.stelib.chab.extrap' : os.path.join( os.path.dirname( GP_GBL.__file__ ),
-#                                               GP_GBL.bc03_stelib_chab_extrap ),
-#     }
-# """ Dictionary providing available SSP-libraries.
-
-# Available SSP formats:
-# ----------------------
-# * bc03.basel.chab.extend :
-# * bc03.stelib.chab.(extend/extrap) :
-# """
-
