a1fff5fee0878b1532234707f23204f3ed1246cc
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2021-11-26 17:02:13 +0100

    added directory containing the filters database to internal.globs

--- galapy/internal/globs.py
+++ galapy/internal/globs.py
@@ -14,12 +14,23 @@ DATA_DIR = 'data'
 BC03     = os.path.join( DATA_DIR, 'BC03' )
 
 # BaSeL (Low Resolution) SSP + Chabrier IMF + null extension
-bc03_basel_chab_zeros  = os.path.join( BC03, 'basel_chab_zero_extended.dat' )
+bc03_basel_chab_zeros  = os.path.join(
+    BC03, 'basel_chab_zero_extended.dat'
+)
 
 # Stelib (High Resolution) SSP + Chabrier IMF + null extension
-bc03_stelib_chab_zeros  = os.path.join( BC03, 'stelib_chab_zero_extended.dat' )
+bc03_stelib_chab_zeros  = os.path.join(
+    BC03, 'stelib_chab_zero_extended.dat'
+)
 
 # Stelib (High Resolution) SSP + Chabrier IMF + extrapolation
-bc03_stelib_chab_extrap = os.path.join( BC03, 'stelib_chab_extrap.dat' )
+bc03_stelib_chab_extrap = os.path.join(
+    BC03, 'stelib_chab_extrap.dat'
+)
+
+################################################################
+# Photometric Filters
+
+FILT = os.path.join( DATA_DIR, 'filters' )
 
 ################################################################
