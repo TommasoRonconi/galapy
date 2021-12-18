86f22390fc05496d50cc8a58ddb9e5c8b19462fb
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2021-12-18 21:47:39 +0100

    added docstrings

--- galapy/StarFormationHistory.py
+++ galapy/StarFormationHistory.py
@@ -1,3 +1,6 @@
+""" GalaPy module handling Star Formation History (SFH) models.
+"""
+
 # External imports
 import numpy
 
@@ -84,7 +87,8 @@ def sfh_build_params ( tau_quench = 2.e+10, model = 'insitu', **kwargs ) :
         
 
 class SFH () :
-    """ Class wrapping the C-core implementation of the Star Formation History type.    
+    """ Class wrapping the C-core implementation of the Star Formation History type.   
+ 
     The possible models to choose are:
 
     #. 'insitu'
@@ -117,14 +121,7 @@ class SFH () :
         self.tunable.remove('model')
         self.set_parameters()
 
-        # steal docstrings from C-core:
-        self.__call__.__func__.__doc__ = self.core.__call__.__doc__
-        self.Mstar.__func__.__doc__    = self.core.Mstar.__doc__
-        self.Mdust.__func__.__doc__    = self.core.Mdust.__doc__
-        self.Mgas.__func__.__doc__     = self.core.Mgas.__doc__
-        self.Zgas.__func__.__doc__     = self.core.Zgas.__doc__
-        self.Zstar.__func__.__doc__    = self.core.Zstar.__doc__
-        
+        self.Mstar.__func__.__doc__    = self.core.Mstar.__doc__        
 
     def __call__ ( self, tau ) :
         """ Method for returning the Star Formation Rate (SFR) at given age.
@@ -179,13 +176,28 @@ class SFH () :
         return;
 
     def Mstar ( self, tau, npoints = 100 ) :
-        """ 
+        """ Computes the stellar mass at a given age of the galaxy.
+        
+        It approximates the integral:
+
+        .. math::
+        
+          M_\\ast(\\tau') = \\int_0^{\\tau'}\\text{d}\\tau 
+                            \\bigl[1 - \\mathcal{R}_\\text{IMF}(\\tau)\\bigr]\\psi(\\tau)
 
         Parameters
         ----------
-        tau : array-like or float
-        
+        tau : float or array-like of floats
+          galaxy age in years.
+        npoints : int
+          thinness for approximated integral computation (default is 100)
+
+        Returns
+        -------
+        : float or array-like of floats
+          the stellar mass of the galaxy at time :math:`\\tau`        
         """
+
         tau = numpy.asarray( tau )
         scalar_input = False
         if tau.ndim == 0 :
@@ -194,11 +206,44 @@ class SFH () :
         ret = numpy.asarray( [ self.core.Mstar( _t, npoints )
                                for _t in tau ],
                              dtype=numpy.float64 )
+        
         if scalar_input :
             return ret.item()
         return ret
 
     def Mdust ( self, tau ) :
+        """ Returns the dust mass at a given age of the galaxy.
+  
+        For empirical models of star formation (i.e. :code:`const`, 
+        :code:`delayedexp`, :code:`lognorm`) this is a free parameter.
+
+        For the :code:`insitu` model, the dust mass is given by 
+
+        .. math::
+           
+           M_\\text{dust} = M_\\text{gas}(\\tau)D(\\tau)
+
+        where 
+
+        .. math::
+           
+           M_\\text{gas}=\\psi(\\tau)\\tau_\\ast
+ 
+        and where :math:`D(\\tau)` is the gas mass ratio 
+        (for an analytic expression of this quantity 
+        see Pantoni et al. 2019 and Lapi et al. 2020).
+
+        Parameters
+        ----------
+        tau : float or array-like of floats
+          age of the galaxy in years.
+
+        Returns
+        -------
+        : float or array-like of floats
+          Dust content in solar masses (:math:`M_\\odot`) at give time :math:`\\tau`.
+        """
+        
         tau = numpy.asarray( tau )
         scalar_input = False
         if tau.ndim == 0 :
@@ -207,11 +252,42 @@ class SFH () :
         ret = numpy.asarray( [ self.core.Mdust( _t )
                                for _t in tau ],
                              dtype=numpy.float64 )
+        
         if scalar_input :
             return ret.item()
         return ret
 
     def Mgas ( self, tau ) :
+        """ Returns the gas mass at a given age of the galaxy.
+
+        For empirical models of star formation (i.e. :code:`const`, 
+        :code:`delayedexp`, :code:`lognorm`) 
+        this is given by 
+
+        .. math::
+           
+           M_\\text{gas} = M_\\text{dust}/D
+        
+        where :math:`D \\sim 0.01 (Z_\\text{gas}/Z_\\odot)^{-0.85}` is the 
+        dust-to-gas mass ratio, derived from observations.
+
+        For the :code:`insitu` model, the gas mass is given by
+
+        .. math::
+           
+           M_\\text{gas}=\\psi(\\tau)\\tau_\\ast
+
+        Parameters
+        ----------
+        tau : float or array-like of floats
+          age of the galaxy in years.
+
+        Returns
+        -------
+        : float or array-like of floats 
+          Gas content in solar masses (:math:`M_\\odot`) at give time :math:`\\tau`.
+        """
+        
         tau = numpy.asarray( tau )
         scalar_input = False
         if tau.ndim == 0 :
@@ -220,11 +296,41 @@ class SFH () :
         ret = numpy.asarray( [ self.core.Mgas( _t )
                                for _t in tau ],
                              dtype=numpy.float64 )
+        
         if scalar_input :
             return ret.item()
         return ret
 
     def Zgas ( self, tau ) :
+        """ Returns the gas metallicity at a given age of the galaxy.
+        
+        For empirical models of star formation (i.e. :code:`const`, 
+        :code:`delayedexp`, :code:`lognorm`) this is a free parameter with 
+        :math:`Z_\\text{gas} = Z_\\ast`.
+
+        For the :code:`insitu` model, it is instead given by
+
+        .. math::
+
+          Z_\\text{gas}=\\dfrac{s y_Z}{s\\gamma-1}
+                        \\biggl[
+                           1 - \\dfrac{(s\\gamma-1)x}{e^{(s\\gamma-1)x}-1}
+                        \\biggr]
+    
+        where :math:`x\\equiv\\tau/s\\tau_\\ast` and :math:`y_Z\\approx0.04` 
+        is the metal production yield (including recycling) for a Chabrier IMF.
+
+        Parameters
+        ----------
+        tau : float or array-like of floats
+          age of the galaxy in years.
+    
+        Returns
+        -------
+        : float or array-like of floats
+          Gas absolute metallicity at give time :math:`\\tau`.
+        """
+    
         tau = numpy.asarray( tau )
         scalar_input = False
         if tau.ndim == 0 :
@@ -233,11 +339,41 @@ class SFH () :
         ret = numpy.asarray( [ self.core.Zgas( _t )
                                for _t in tau ],
                              dtype=numpy.float64 )
+        
         if scalar_input :
             return ret.item()
         return ret
 
     def Zstar ( self, tau ) :
+        """Returns the stellar metallicity at a given age of the galaxy.
+
+        For empirical models of star formation (i.e. :code:`const`, 
+        :code:`delayedexp`, :code:`lognorm`) this is a free parameter with 
+        :math:`Z_\\ast = Z_\\text{gas}`.
+
+        For the :code:`insitu` model, it is instead given by
+
+        .. math::
+
+          Z_\\ast=\\dfrac{y_Z}{\\gamma-1}
+                  \\biggl[1 - \\dfrac{s\\gamma}{(s\\gamma-1}
+                  \\dfrac{e^{-x}-e^{-s\\gamma x}[1 + (s\\gamma -1)x]}
+                  {s\\gamma -1 + e^{-s\\gamma x}- s\\gamma e^{-x}}\\biggr]
+
+        where :math:`x\\equiv\\tau/s\\tau_\\ast` and :math:`y_Z\\approx0.04` 
+        is the metal production yield (including recycling) for a Chabrier IMF.
+
+        Parameters
+        ----------
+        tau : float or array-like of floats
+          age of the galaxy in years.
+
+        Returns
+        -------
+        : float or array-like of floats
+          Stellar absolute metallicity at give time :math:`\\tau`.
+        """
+        
         tau = numpy.asarray( tau )
         scalar_input = False
         if tau.ndim == 0 :
@@ -246,6 +382,7 @@ class SFH () :
         ret = numpy.asarray( [ self.core.Zstar( _t )
                                for _t in tau ],
                              dtype=numpy.float64 )
+        
         if scalar_input :
             return ret.item()
         return ret
