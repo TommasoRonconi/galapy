114d03efcb41bee7a9cd55bd1169b7e66a17a295
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2022-01-20 15:28:24 +0100

    added module Galaxy to the ones authomatically loaded when importing galapy

--- galapy/__init__.py
+++ galapy/__init__.py
@@ -4,5 +4,6 @@
 import galapy.StarFormationHistory
 import galapy.CompositeStellarPopulation
 import galapy.InterStellarMedium
+import galapy.Galaxy
 
 __all__ = [ 'Galaxy', 'StarFormationHistory', 'CompositeStellarPopulation', 'InterStellarMedium' ]
