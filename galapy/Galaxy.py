4f554a0af195b98c02bd511b0dd713c89dce9678
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2022-02-07 11:37:29 +0100

    modified photoGXY constructor (taking args & kwargs authomatically)

--- galapy/Galaxy.py
+++ galapy/Galaxy.py
@@ -176,14 +176,20 @@ class GXY () :
 
 class PhotoGXY ( GXY ) :
 
-    def __init__ ( self, age, redshift, lstep = None, cosmo = 'Planck18', 
-                   sfh_kw = {}, csp_kw = {}, ism_kw = {} ) :
-        super().__init__( age, redshift,
-                          lstep=lstep, cosmo=cosmo,
-                          sfh_kw=sfh_kw,
-                          csp_kw=csp_kw,
-                          ism_kw=ism_kw )
+    def __init__ ( self, *args, **kwargs ) :
+
+        super().__init__( *args, **kwargs )
         self.pms = None
+
+    # def __init__ ( self, age, redshift, lstep = None, cosmo = 'Planck18', 
+    #                sfh_kw = {}, csp_kw = {}, ism_kw = {}, agn_kw = None ) :
+    #     super().__init__( age, redshift,
+    #                       lstep=lstep, cosmo=cosmo,
+    #                       sfh_kw=sfh_kw,
+    #                       csp_kw=csp_kw,
+    #                       ism_kw=ism_kw,
+    #                       agn_kw=agn_kw )
+    #     self.pms = None
 
     def build_photometric_system ( self, *args, **kwargs ) :
 
