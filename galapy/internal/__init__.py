53ac7dc65b7912561c41820e1bc2c78f810cf2fe
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2022-05-28 13:30:41 +0200

    Added and modified functionalities for managing data internally.
    - new module `data` implements routines for managing data-files, namely:
      * `download` and `is_url` functions retrieve from a given url a file.
      * the DataFile class deals with the research in the system or from
        the internet of data-files, the function `get_file()` provides the
        cross-platform path to the file after it has been found/downloaded
    - modified the globs to be compliant with the new data-system

--- galapy/internal/__init__.py
+++ galapy/internal/__init__.py
@@ -1,5 +1,6 @@
 import galapy.internal.utils
 import galapy.internal.globs
 import galapy.internal.constants
+import galapy.internal.data
 
-__all__ = [ 'globs', 'utils', 'constants' ]
+__all__ = [ 'globs', 'utils', 'constants', 'data' ]
--- galapy/internal/data.py
+++ galapy/internal/data.py
@@ -1,8 +1,12 @@
+# External imports
 import os
+import warnings
 import requests
 import urllib.parse
 
-# base_url = "https://raw.githubusercontent.com/TommasoRonconi/galapy/tree/main/galapy/internal/data"
+# Internal imports
+from galapy.configuration import rcParams
+from galapy.internal.globs import *
 
 def is_url ( string ) :
     """
@@ -18,9 +22,9 @@ def is_url ( string ) :
     status : bool
         String is URL or not.
 
-    Warning
-    -------
-    This function has been stolen from the Astropy Collaboration
+    Notes
+    -----
+    This routine has been adapted from the Astropy Collaboration (http://www.astropy.org)
     """
     u = urllib.parse.urlparse(string)
     # we can't just check that url.scheme is not an empty string, because
@@ -28,7 +32,7 @@ def is_url ( string ) :
     # returns 'e').
     return u.scheme.lower() in ['http', 'https', 'ftp', 'sftp', 'ssh', 'file']
 
-def download_file ( url, out_file, force = False ) :
+def download_file ( url, out_file, overwrite = False, verbose = True ) :
     """ Download file from known url into defined position
 
     Parameters
@@ -37,16 +41,115 @@ def download_file ( url, out_file, force = False ) :
       The complete url of the file to download
     out_file : str
       Absolute path to output file
+    overwrite : bool
+      If set to True, the eventually already existing out_file is overwritten
+    verbose : bool
+      If set to True, prints additional informations while processing
 
     Returns
     -------
     : int
-    size in bytes of the out_file
+    size in bytes written to the out_file
+
+    Raises
+    ------
+    OSError 
+      If the input url string is not a valid URL. 
+      If the output file directory does not exist and the system is not able to create it.
+    requests.exceptions.HTTPError
+      If an HTTP error occurred.
+    Exception
+      If some other error occurs.
     """
 
     if not is_url( url ) :
-        raise OSError( f"URL {url:s} does not link to any valid file." )
-    
-    r = requests.get( url )
-    return open(out_file, "wb").write(r.content)
+        raise OSError( f"The string provided {url:s} is not a valid URL." )
 
+    # Extracting bytes from the provided URL:
+    _bytes = b''
+    try :
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as http_err :
+        print( f'HTTP error occurred: {http_err}' )
+    except Exception as err :
+        print( f'Other error occurred: {err}' )
+    else :
+        _bytes = response.content
+
+    # If overwrite is set to False
+    # and the path already exist leave the file as is
+    # returns zero (number of bytes written)
+    if os.path.exists( out_file ) and not overwrite :
+        warnings.warn( f"File {out_file} already exists." )
+        return 0
+        
+    # Checking whether the directory for file output is accessible 
+    out_dir = os.path.dirname( out_file )
+    if not os.path.isdir( out_dir ) :
+        if verbose : print( f"Directory {out_dir} does not exist, it will be created." )
+        try :
+            # creates multi-level subdirs. similarly to the *Nix command `mkdir -p`
+            # (while os.mkdir() only allows to create the highest level directory)
+            os.makedirs(path) 
+        except OSError:
+            print ( f"Creation of the directory {out_dir} failed" )
+        else :
+            if verbose : print( f"Successfully created the directory {out_dir}" )
+
+    return open(out_file, "wb").write(_bytes)
+
+class DataFile () :
+    """
+    - search in the rcParams[ 'datapath' ] list
+    - if not present, download and store it 
+    """
+
+    def __init__ ( self, file_name, path_line ) :
+
+        # allocate variable to store the file-path
+        self._filepath = None
+
+        # search in the available data-paths for the requested file
+        for datapath in rcParams[ 'datapath' ] :
+            _testpath = os.path.join( datapath, *path_line, file_name )
+            if not os.path.exists( _testpath ) :
+                continue
+            elif os.path.isdir( _testpath ) :
+                raise OSError( f'The DataFile {file_name} '
+                               f'in position {os.path.join(datapath, *path_line)} '
+                               'is a directory.' )
+            else :
+                self._filepath = _testpath
+
+        # if the data-file is not present in the system download it from remote
+        if self._filepath is None :
+            where = os.path.join( rcParams[ 'datapath' ][ 0 ],
+                                  *path_line,
+                                  file_name )
+            try :
+                byte_count = download(
+                    url = DATA_URL.format( '/'.join( *path_line, file_name ) ),
+                    out_file = where,
+                    overwrite = rcParams[ 'force_downloads' ],
+                    verbose = rcParams[ 'verbose_downloads' ]
+                )
+            except Exception as err :
+                print( f'An error occurred: {err}' )
+                raise
+            else :
+                if rcParams[ 'verbose_downloads' ] : print( f'Downloaded {byte_count} bytes.' )
+                self._filepath = where
+            
+
+    def get_file ( self ) :
+        """ 
+        - return the string with the cross-platform path
+        """
+        return self._filepath
+
+# def search_file () :
+#     pass
+
+# def download_database ( loc = None ) :
+#     pass
