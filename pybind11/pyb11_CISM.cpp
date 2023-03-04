36d15edafadab822c971af133a727d5c2491f4d1
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-03-04 16:56:38 +0000

    maybe this time the buggy CMC.temperature function it is solved for real?

--- pybind11/pyb11_CISM.cpp
+++ pybind11/pyb11_CISM.cpp
@@ -20,21 +20,22 @@ PYBIND11_MODULE( ISM_core, m ) {
   py::class_< sed::diffuse >( m, "CDD" )
     .def( py::init<>() )
     .def( "set_params",
-	  [] ( sed::diffuse & dd,
+	  [] ( sed::diffuse & o,
 	       const py::array_t< double > & params ) {
-	    dd.set_params( static_cast< const double * >( params.data() ) );
+	    o.set_params( static_cast< const double * >( params.data() ) );
 	  } )
     .def( "set_temperature", &sed::diffuse::set_temperature,
 	  "Sets internally the temperature" )
     .def( "set_slopes",  &sed::diffuse::set_slopes )
-    .def( "temperature", &sed::diffuse::temperature,
+    .def( "temperature", []( sed::diffuse & o, const double T ){
+			   return o.temperature( T ); },
 	  "Computes temperature necessary for "
 	  "energy balance and returns it",
 	  py::arg("Etot") )
     .def( "extinction",  py::vectorize(&sed::diffuse::extinction) )
     .def( "attenuation", py::vectorize(&sed::diffuse::attenuation) )
     .def( "emission",    py::vectorize(&sed::diffuse::emission) )
-    .def( "A_V", []( const sed::diffuse & dd ){ return dd.get_params()[0]; }  )
+    .def( "A_V", []( const sed::diffuse & o ){ return o.get_params()[0]; }  )
     .def(py::pickle(
     		    []( const sed::diffuse &o ) { //__getstate__
 		      return utl::__getstate__< sed::diffuse >( o ); },
@@ -47,14 +48,15 @@ PYBIND11_MODULE( ISM_core, m ) {
   py::class_< sed::cloud >( m, "CMC" )
     .def( py::init<>() )
     .def( "set_params",
-	  [] ( sed::cloud & dd,
+	  [] ( sed::cloud & o,
 	       const py::array_t< double > & params ) {
-	    dd.set_params( static_cast< const double * >( params.data() ) );
+	    o.set_params( static_cast< const double * >( params.data() ) );
 	  } )
     .def( "set_temperature", &sed::cloud::set_temperature,
 	  "Sets internally the temperature" )
     .def( "set_slopes",  &sed::cloud::set_slopes )
-    .def( "temperature", &sed::cloud::temperature,
+    .def( "temperature", []( sed::cloud & o, const double T ){
+			   return o.temperature( T ); },
 	  "Computes temperature necessary for "
 	  "energy balance and returns it",
 	  py::arg("Etot") )
@@ -62,7 +64,7 @@ PYBIND11_MODULE( ISM_core, m ) {
     .def( "attenuation", py::vectorize(&sed::cloud::attenuation) )
     .def( "emission",    py::vectorize(&sed::cloud::emission) )
     .def( "eta", py::vectorize(&sed::cloud::eta) )
-    .def( "A_V", []( const sed::cloud & dd ){ return dd.get_params()[0]; }  )
+    .def( "A_V", []( const sed::cloud & o ){ return o.get_params()[0]; }  )
     .def(py::pickle(
     		    []( const sed::cloud &o ) { //__getstate__
 		      return utl::__getstate__< sed::cloud >( o ); },
