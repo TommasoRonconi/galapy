66d1ba312abb6521ffcff8528a59d42f558dc3cd
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-05-31 16:26:58 +0200

    removed obsolete commented section

--- galapy/__init__.py
+++ galapy/__init__.py
@@ -3,26 +3,4 @@
 
 from galapy.configuration import rcParams
 
-# import galapy.StarFormationHistory
-# import galapy.CompositeStellarPopulation
-# import galapy.InterStellarMedium
-# import galapy.ActiveGalacticNucleus
-# import galapy.XRayBinaries
-# import galapy.NebularFreeFree
-# import galapy.Synchrotron
-# import galapy.PhotometricSystem
-# import galapy.Galaxy
-
-# __all__ = [
-#     'Galaxy',
-#     'StarFormationHistory',
-#     'CompositeStellarPopulation',
-#     'InterStellarMedium',
-#     'ActiveGalacticNucleus',
-#     'NebularFreeFree',
-#     'Synchrotron',
-#     'XRayBinaries',
-#     'PhotometricSystem'
-# ]
-
 __version__ = '0.2.0'
