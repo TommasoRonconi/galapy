97444f2529a45ab86a6010c59582d1192c9d562a
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-11-02 16:31:46 +0100

    modified dump_to/load_from_hdf5 by adding the recursive method to read and write nested dictionaries to file

--- galapy/io/hdf5.py
+++ galapy/io/hdf5.py
@@ -1,4 +1,43 @@
 import os, h5py, numpy, warnings
+from collections.abc import MutableMapping as MM
+
+def recursive_create ( group, indict ) :
+    """Dumps an input dictionary to a HDF5 file
+    """
+    for k, v in indict.items() :
+        try :
+            if isinstance( v, MM ) :
+                new_group = group.create_group( k )
+                recursive_create( new_group, v )
+            else :
+                if hasattr( v, '__len__' ) and not isinstance( v, str ) :
+                    group.create_dataset( k, data = v )
+                else :
+                    if v is not None :
+                        group.attrs[k] = v
+                    else :
+                        group.attrs[k] = 'none'
+        except Exception as err :
+            print( f"- Skipping field '{k}' as it raises "
+                   f"exception {type(err).__name__} with message:\n  '{err}'" )
+    return;
+
+def recursive_read ( group, outdict ) :
+    """Reads a nested dictionary from a HDF5 file
+    """
+    outdict.update( { k : v if v != 'none' else None for k,v in dict(group.attrs).items()} )
+    for k, v in group.items() :
+        try :
+            if isinstance( v, h5py.Dataset ) :
+                outdict[k] = group.get(k)[:]
+            if isinstance( v, h5py.Group ) :
+                outdict[k] = {}
+                recursive_read( v, outdict[k] )
+        except Exception as err :
+            print( f"- Skipping dataset '{k}' as it raises "
+                   f"exception {type(err).__name__} with message:\n  '{err}'" )
+            
+    return;
 
 def write_to_hdf5 ( outfile, metadata, hard = False, **groups ) :
     """Automatic dump of several dictionaries into hdf5 file as separate groups in file.
@@ -23,12 +62,8 @@ def write_to_hdf5 ( outfile, metadata, hard = False, **groups ) :
     
     See Also
     --------
-    load_from_hdf5 : loads all the groups in a ``.hdf5`` file
+    load_from_hdf5 : loads all the groups in a HDF5 file
     """
-
-    # check right extension
-    if outfile.split('.')[-1] != 'hdf5' :
-        raise AttributeError( 'the provided file does not have a hdf5 extension' )
 
     # check for overwrite
     if not hard and os.path.isfile( outfile ) :
@@ -52,12 +87,7 @@ def write_to_hdf5 ( outfile, metadata, hard = False, **groups ) :
         # store groups:
         for name, dictionary in groups.items() :
             group = f.create_group(name)
-            for key, data in dictionary.items() :
-                try :
-                    group.create_dataset( key, data = data )
-                except Exception as err :
-                    print( f"- Skipping dataset '{key}' of group '{name}' as it raises "
-                           f"exception {type(err).__name__} with message:\n  '{err}'" )
+            recursive_create( group, dictionary )
         
     print( f"Wrote on file {outfile}" )
     return;
@@ -82,8 +112,8 @@ def load_from_hdf5 ( infile ) :
     Warning
     -------
     This is a simplified reader intended to work with hdf5 files generate
-    using the ``scampy.io.hdf5.write_to_hdf5`` method.
-    It is not guaranteed to work with any ``.hdf5``. 
+    using the ``galapy.io.hdf5.write_to_hdf5`` method.
+    It is not guaranteed to work with any HDF5. 
     It assumes that at the top level all the fields can be cast to dictionaries.
     If this is not possible it raises a warning.
     """
@@ -100,13 +130,8 @@ def load_from_hdf5 ( infile ) :
 
         # load groups
         for name, todict in f.items() :
-            try :
-                outdict[name] = { k : todict.get(k)[:] for k in todict.keys() }
-            except Exception as err :
-                warnings.warn(
-                    f"- Skipping group '{name}' as it raises "
-                    f"exception {type(err).__name__} with message:\n  '{err}'"
-                )
+            outdict[name] = {}
+            recursive_read( todict, outdict[name] )
             
     print( f"Read from file {infile}" )
     return outdict
