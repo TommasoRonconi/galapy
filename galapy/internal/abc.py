690b24dc67e85635a9ddfce0bb3084372d161515
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2023-11-02 16:30:21 +0100

    added abstract object GalaPyObject, not used yet anywhere but in the future it will become mandatory as basic inheritance

--- galapy/internal/abc.py
+++ galapy/internal/abc.py
@@ -1,6 +1,38 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractclassmethod
 
-class Model (ABC) :
+class GalaPyObject ( ABC ) :
+    
+    @abstractmethod
+    def _dump_hdf5 ( self, outfile ) :
+        pass
+
+    @abstractclassmethod
+    def _load_hdf5 ( cls, indict ) :
+        pass
+    
+    def dump ( self, outfile, method = 'hdf5' ) :
+        if method == 'hdf5' :
+            self._dump_hdf5( outfile )
+            return;
+        if method == 'pickle' :
+            import pickle
+            with open( outfile, 'wb' ) as f :
+                pickle.dump( self, f )
+            return;
+        return;
+
+    @classmethod
+    def load ( cls, infile, method = 'hdf5' ) :
+        if method == 'hdf5' :
+            from galapy.io import load_from_hdf5
+            return _load_hdf5( load_from_hdf5( infile ) )
+        if method == 'pickle' :
+            import pickle
+            with open( infile, 'rb' ) as f :
+                return pickle.load( f )
+
+#class Model ( GalaPyObject ) :
+class Model ( ABC ) :
     
     def __init__ ( self ) :
         """ Abstract base interface for all the objects that 
@@ -11,5 +43,3 @@ class Model (ABC) :
     @abstractmethod
     def set_parameters ( self ) :
         pass
-
-
